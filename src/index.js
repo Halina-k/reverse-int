19f481b4b73470260e205197e8879c8234e2961e
Halina Kulakova <kalisakulakova@gmail.com>
2022-01-12 18:49:32 +0100

    feat: create function reverse

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
+  let i = 0, result = '';
   
+  n = Math.abs(n).toString();
+  
+  while (i < n.length) {
+      result += n[n.length - i - 1];
+      i++;
+  }
+  return result * 1;
 }
+  
